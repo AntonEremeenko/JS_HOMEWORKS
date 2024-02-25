658533d287dbb0bfd3837da15f0cdf40f2cb045d
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-02-25 18:40:41 +0200

    hw_7 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Task #1
+
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+alert(admin);
+
+// Task #2
+
+const yourPlanetName = 'Earth';
+const userName = 'John';
+
+// Task #3
+
+const sum = '10' + 10;
+console.log(typeof sum);
