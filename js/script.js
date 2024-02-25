ffec711efa242b35a2dc1cc8fcdaf29420ed1934
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-02-25 18:22:16 +0200

    hw_6 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const  name = prompt('Enter your first name');
+const  lastName = prompt('Enter your last name');
+const  middleName = prompt('Enter your middle name');
+const userFullName = lastName + ' ' + name + ' ' + middleName;
+console.log(userFullName);
+
+const number = 12345;
+const numberString = number.toString();
+const result = `${numberString[0]} ${numberString[1]} ${numberString[2]} ${numberString[3]} ${numberString[4]}`;
+console.log(result);
