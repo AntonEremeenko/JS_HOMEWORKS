41de8d7edaf7cfcabf52deac10da526c4261b8a5
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-02-28 22:07:51 +0200

    hw_9 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function convertTemperature() {
+    const temperature = prompt('Enter a number');
+    const temperatureNum = Number(temperature);
+
+    if (typeof temperatureNum !=='number') {
+        alert('Please enter a valid number.');
+        return convertTemperature();
+    }
+
+    const celsiusOrFahrenheit = prompt('Enter C (Celsius) or F (Fahrenheit)');
+
+    if (celsiusOrFahrenheit.toUpperCase() === 'C') {
+        const fahrenheit = 9 / 5 * temperatureNum + 32;
+        alert(`${fahrenheit}F`);
+    } else if (celsiusOrFahrenheit.toUpperCase() === 'F') {
+        const celsius = 5 / 9 * (temperatureNum - 32);
+        alert(`${celsius}C`);
+    } else {
+        alert('Please enter C for Celsius or F for Fahrenheit.');
+    }
+}
+convertTemperature();
