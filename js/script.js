8e4909afc09ca2ca6778f29af811feb657a83424
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-01 10:46:48 +0200

    hw_10 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const operand1 = +prompt('Enter the first number:');
+const operand2 = +prompt('Enter second number:');
+
+if (!isNaN(operand1) && !isNaN(operand2)) {
+
+    console.log(`Результат: ${operand1} + ${operand2} = ${operand1 + operand2}`);
+    console.log(`Результат: ${operand1} - ${operand2} = ${operand1 - operand2}`);
+    console.log(`Результат: ${operand1} * ${operand2} = ${operand1 * operand2}`);
+    console.log(`Результат: ${operand1} / ${operand2} = ${operand1 / operand2}`);
+} else {
+    console.log('Something is wrong, please enter numbers.');
+}
