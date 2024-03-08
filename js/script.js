a0387b3d9dad0daf0775125372de8119d6543a10
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-08 22:49:46 +0200

    hw_21 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,87 @@
 'use strict';
+
+// 1.
+// const a = +prompt();
+// const result = a === 0 ? 'Вірно' : 'Неправильно';
+// console.log(result);
+
+// 2.
+// const a = +prompt();
+// const result = a > 0 ? 'Вірно' : 'Неправильно';
+// console.log(result);
+
+// 3.
+// const a = +prompt();
+// const result = a < 0 ? 'Вірно' : 'Неправильно';
+// console.log(result);
+
+//  4.
+// const a = +prompt();
+// const result = a >= 0 ? 'Вірно' : 'Неправильно';
+// console.log(result);
+
+// 5.
+// const a = +prompt();
+// const result = a <= 0 ? 'Вірно' : 'Неправильно';
+// console.log(result);
+
+// 6.
+// const a = +prompt();
+// const result = a !== 0 ? 'Вірно' : 'Неправильно';
+// console.log(result);
+
+// 7.
+// const a = prompt();
+// const result = a === 'test' ? 'Вірно' : 'Неправильно';
+// console.log(result);
+
+
+// 8.
+// const a = prompt();
+// const result = a ===  '1' ? 'Вірно' : 'Неправильно';
+// console.log(result);
+
+// 9.
+// const a = prompt();
+// const result = a > 0 ?
+//     a < 5  ? 'Вірно' : 'Неправильно' : 'Неправильно' ;
+// console.log(result);
+
+// 10.
+// const a = +prompt();
+// const result = (a === 0 || a === 2)  ? a + 7 : a / 10 ;
+// console.log(result);
+
+// 11.
+// const a = +prompt('a');
+// const b = +prompt('b');
+// const result = (a <= 1 && b >= 3) ? a + b : a - b ;
+// console.log(result);
+
+// 12.
+// const a = +prompt('a');
+// const b = +prompt('b');
+// const result = (a > 2  && a < 11) || (b >=6 && b < 14) ? 'Вірно' : 'Невірно' ;
+// console.log(result);
+
+// 13.
+// const num = +prompt('Введіть число від 1го до 4х');
+// let result;
+//
+// switch (num) {
+//     case 1:
+//          result = 'зима';
+//         break;
+//     case 2:
+//          result = 'весна';
+//         break;
+//     case 3:
+//          result = 'літо';
+//         break;
+//     case 4:
+//          result = 'осінь';
+//          break;
+//     default:
+//         result = 'Введіть число від 1го до 4х';
+// }
+// console.log(result);
