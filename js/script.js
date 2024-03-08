2d6d2875f72a3def2f66e3325cba1b8e75ca5f1a
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-08 23:26:44 +0200

    hw_19 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+let age = prompt('Введіть ваш вік:');
+
+if (age === null) {
+    alert('Шкода, що Ви не захотіли ввести свій вік');
+} else {
+    age = +age;
+
+    if (!isNaN(age)) {
+        let word = 'років';
+
+        if (age % 10 === 1 && age % 100 !== 11) {
+            word = 'рік';
+        } else if (age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
+            word = 'роки';
+        }
+
+        alert(age + " " + word);
+    } else {
+        alert('Будь ласка, введіть коректне число.');
+    }
+}
