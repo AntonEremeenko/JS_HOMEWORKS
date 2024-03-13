f4b61737cd0de1f53c40cf27c8a0cc17ba355779
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-13 16:43:57 +0200

    hw_24 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// 1.
+// const arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+// console.log(sum);
+
+// 2.
+// const arr = [1, 2, 3, 4, 5];
+// let sumOfSquares = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sumOfSquares += arr[i]**2;
+// }
+// console.log(sumOfSquares);
