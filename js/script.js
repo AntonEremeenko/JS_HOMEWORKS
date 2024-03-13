f337f27de90a2ac60a367bb8a85a3071553f35b0
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-13 23:41:31 +0200

    hw_24 deleted comment

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 'use strict';
 
-// 1.
-// const arr = [1, 2, 3, 4, 5];
-// let sum = 0;
-// for (let i = 0; i < arr.length; i++) {
-//     sum += arr[i];
-// }
-// console.log(sum);
 
-// 2.
-// const arr = [1, 2, 3, 4, 5];
-// let sumOfSquares = 0;
-// for (let i = 0; i < arr.length; i++) {
-//     sumOfSquares += arr[i]**2;
-// }
-// console.log(sumOfSquares);
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+console.log(sum);
+
+
+const arr1 = [1, 2, 3, 4, 5];
+let sumOfSquares = 0;
+for (let i1 = 0; i1 < arr1.length; i1++) {
+    sumOfSquares += arr1[i1]**2;
+}
+console.log(sumOfSquares);
