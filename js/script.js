ea49dda6cbb3222f98cc1c0cb1f09ee8da130ba1
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-13 23:28:39 +0200

    hw_22 deleted comment

--- js/script.js
+++ js/script.js
@@ -1,98 +1,98 @@
 'use strict';
 
-// 1.
-// let i = 10;
-// let str = '';
-// for ( ; i < 20; i++) {
-//
-//     str = str + i.toString();
-//     if (i < 19) {
-//         str = str  + ',';
-//     }
-//
-// }
-// console.log(str);
 
-// 2.
-// for (let i = 10; i < 20; i++) {
-//     const a = i ** 2;
-//     console.log(a);
-// }
+let i = 10;
+let str = '';
+for ( ; i < 20; i++) {
 
-// 3.
-// for (let i = 1; i < 10; i++) {
-//     const result = i * 7
-//     console.log(`${i} * 7 = ${result}`);
-// }
+    str = str + i.toString();
+    if (i < 19) {
+        str = str  + ',';
+    }
 
-// 4.
-// let sum = 0;
-// for (let i = 1; i <= 15; i++) {
-//  sum = sum + i;
-// }
-// console.log(sum);
+}
+console.log(str);
 
-// 5.
-// let sum = 1;
-// for (let i = 15; i <= 35; i++) {
-//  sum = sum * i;
-//     console.log(sum);
-// }
 
-// 6.
-// let sum = 0;
-// for (let i = 1; i <= 500; i++) {
-//     sum = (sum + i);
-// }
-//     console.log(sum / 500);
+for (let c = 10; c < 20; c++) {
+    const a = c ** 2;
+    console.log(a);
+}
 
-// 7.
-// let sum = 0;
-// for (let i = 30; i <= 80; i ++) {
-//     if (i % 2 === 0) {
-//         sum += i;
-//     }
-// }
-// console.log(sum);
 
-// 8.
-// for (let i = 100; i <= 200; i ++) {
-//     if (i % 3 === 0) {
-//         console.log(i);
-//     }
-// }
+for (let j = 1; j < 10; j++) {
+    const result = j * 7
+    console.log(`${j} * 7 = ${result}`);
+}
 
-// 9.
-// const num = +prompt('Напишіть натуральне число');
-//     for (let i = 1; i <= num; i++) {
-//         if (num % i === 0) {
-//             console.log(i);
-//         }
-// }
 
-// 10.
-// const num = +prompt('Напишіть натуральне число');
-// let countEvenDivisors = 0;
-// for (let i = 1; i <= num; i++) {
-//     if (num % i === 0 && i % 2 === 0) {
-//         countEvenDivisors++;
-//     }
-// }
-// console.log(countEvenDivisors);
+let sum = 0;
+for (let b = 1; b <= 15;b++) {
+ sum = sum + b;
+}
+console.log(sum);
 
-// 11.
-// const num = +prompt('Напишіть натуральне число');
-// let sumEvenDivisors = 0;
-// for (let i = 1; i <= num; i++) {
-//     if (num % i === 0 && i % 2 === 0) {
-//         sumEvenDivisors += i;
-//     }
-// }
-// console.log(sumEvenDivisors);
 
-// 12.
-// for (let i = 1; i < 10; i++) {
-//     for (let j = 1; j < 10; j++) {
-//         console.log(`${j} * ${i} = ${i * j}`)
-//     }
-// }
+let sum1 = 1;
+for (let f = 15; f <= 35; f++) {
+ sum1 = sum1 * f;
+    console.log(sum1);
+}
+
+
+let sum2 = 0;
+for (let z = 1; z <= 500; z++) {
+    sum2 = (sum2 + z);
+}
+    console.log(sum2/ 500);
+
+
+let sum3 = 0;
+for (let v = 30; v <= 80; v++) {
+    if (i % 2 === 0) {
+        sum3 += v;
+    }
+}
+console.log(sum3);
+
+
+for (let r = 100; r <= 200; r++) {
+    if (r % 3 === 0) {
+        console.log(r);
+    }
+}
+
+
+const num = +prompt('HW9 Напишіть натуральне число');
+    for (let e = 1; e <= num; e++) {
+        if (num % e === 0) {
+            console.log(e);
+        }
+}
+
+
+const num1 = +prompt('HW10 Напишіть натуральне число');
+let countEvenDivisors = 0;
+for (let q = 1; q <= num1; q++) {
+    if (num1 % q === 0 && q % 2 === 0) {
+        countEvenDivisors++;
+    }
+}
+console.log(countEvenDivisors);
+
+
+const num2 = +prompt('HW11 Напишіть натуральне число');
+let sumEvenDivisors = 0;
+for (let h = 1; h <= num2; h++) {
+    if (num2 % h === 0 && h % 2 === 0) {
+        sumEvenDivisors += h;
+    }
+}
+console.log(sumEvenDivisors);
+
+
+for (let y = 1; y < 10; y++) {
+    for (let u = 1; u < 10; u++) {
+        console.log(`${u} * ${y} = ${y * u}`)
+    }
+}
