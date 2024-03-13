c469b822a412a9457f0c0a455fd856cfad3dc0ac
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-13 19:42:07 +0200

    hw_25 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const num = +prompt('Введіть номер');
+if (num < 1) {
+    console.log('NaN');
+} else {
+    for (let i = 2; i <= num; i++) {
+        if (num % i === 0) {
+            console.log(i);
+            break;
+        }
+    }
+}
