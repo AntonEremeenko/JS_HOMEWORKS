b06e5e518c281594df47a90ddc1d45f37768b2d6
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-15 18:35:30 +0200

    hw_30 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+const filterPositiveNumbers = function (arr) {
+    const positiveArr = [];
+
+    if (arr.length === 0) {
+        return 'Порожній масив';
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positiveArr.push(arr[i]);
+        }
+    }
+
+    if (positiveArr.length > 0) {
+        return positiveArr;
+    } else {
+        return null;
+    }
+}
+
+console.log(filterPositiveNumbers(arr));
