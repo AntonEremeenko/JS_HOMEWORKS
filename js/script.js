4362a7cb2e81ede0b36351008527b93d391ca259
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-15 16:46:23 +0200

    hw_28 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const number = +prompt('Enter number');
+const power = +prompt('Enter power');
+
+const numberPower = function (number, power = 1) {
+    if (isNaN(number) || isNaN(power)) {
+        return 'помилка';
+    } else {
+        return Math.pow(number, power);
+    }
+}
+const result = numberPower(number, power);
+alert(result);
