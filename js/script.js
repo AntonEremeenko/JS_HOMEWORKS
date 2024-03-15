12313e277db61d223aaff7bdcf3915493d7e07f9
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-03-15 18:06:38 +0200

    hw_29 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const padString = function (inputString, finalLength, symbol = '', padRight = true) {
+    if (typeof inputString !== 'string') return 'відсутній рядок';
+    if (typeof finalLength !== 'number') return 'відсутня цифра';
+    if (typeof symbol === undefined || symbol.length !== 1) return 'відсутній cимвол';
+
+    if (inputString.length > finalLength) {
+        return inputString.slice(0, finalLength);
+    }
+
+    const padLangth = finalLength - inputString.length;
+    const padding = symbol.repeat(padLangth);
+    return padRight ? inputString + padding : padding + inputString;
+}
+
+console.log(padString('hello', 8, '#', true));
