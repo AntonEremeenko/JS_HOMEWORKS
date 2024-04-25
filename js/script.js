77039e0de48ee3ad1c8a7e5b67df6c48bb2a1cd5
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-04-25 19:44:52 +0300

    hw_57 changed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-const getSum = () => {
-    let value = null;
-    return function (numb) {
-        if (value === null) {
-            value = numb;
-            return value;
-        }
-        value += numb;
-        return value;
+(function () {
+    const getSum = (numb) => {
+        let result = numb;
+
+        return (value) => {
+            result += value;
+            return result;
+        };
     };
-};
-const sum = getSum();
-console.log(sum(4));
-console.log(sum(4));
-console.log(sum(4));
+    const sum = getSum(10);
+    console.log(sum(20));
+})();
