b0afa3010ed9c23d77b9c82761f8b6f9fd1224e2
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-05-16 19:32:20 +0300

    hw_63 completed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,67 @@
-'use strict';
+(function () {
+    const selectListener = function (event) {
+        const parentBlock = event.target.closest('div[class]');
+        let block = null;
+        if (parentBlock.classList.contains('block1')) {
+            block = document.getElementsByClassName('block2')[0];
+            const shownElements = block.getElementsByClassName('shown');
+            for (let e of shownElements) {
+                e.classList.remove('shown');
+            }
+        }
+        // if (parentBlock.classList.contains('block2')) {
+        //     block = document.getElementsByClassName('block3')[0];
+        // }
 
-const operand1 = +prompt('Enter the first number:');
-const operand2 = +prompt('Enter second number:');
+        const idList = event.target.dataset.nextBlock.split(' ');
+        for (let id of idList) {
+            document.getElementById(id).className = 'shown';
+        }
+    };
 
-if (!isNaN(operand1) && !isNaN(operand2)) {
-    console.log(
-        `Результат: ${operand1} + ${operand2} = ${operand1 + operand2}`
-    );
-    console.log(
-        `Результат: ${operand1} - ${operand2} = ${operand1 - operand2}`
-    );
-    console.log(
-        `Результат: ${operand1} * ${operand2} = ${operand1 * operand2}`
-    );
-    console.log(
-        `Результат: ${operand1} / ${operand2} = ${operand1 / operand2}`
-    );
-} else {
-    console.log('Something is wrong, please enter numbers.');
-}
+    const items = document.getElementsByClassName('selectable-list');
+    for (let i = 0; i < items.length; i++) {
+        items.item(i).addEventListener('click', selectListener);
+    }
+
+    const hideShown = function () {
+        const elements = document.getElementsByClassName('shown');
+        while (elements.length > 0) {
+            const element = elements.item(0);
+            element.classList.remove('shown');
+        }
+    };
+
+    const buy = function (event) {
+        let boughtProducts = 'You bought:\n';
+
+        const block3 = document.getElementsByClassName('block3')[0];
+
+        for (let e of block3.getElementsByClassName('shown')) {
+            if (e.id !== 'buy-button') {
+                const name = e.getElementsByClassName('name')[0];
+                const description = e.getElementsByClassName('description')[0];
+                boughtProducts +=
+                    name.innerText + ': ' + description.innerText + '\n';
+            }
+        }
+
+        hideShown();
+        // console.log(boughtProducts);
+        alert(boughtProducts);
+    };
+
+    document.getElementById('buy-button').addEventListener('click', buy);
+
+    const textColor = function (event) {
+        event.target.className = 'color';
+    };
+    const out = function (event) {
+        event.target.classList.remove('color');
+    };
+    const liElements = document.getElementsByTagName('li');
+    for (let i = 0; i < liElements.length; i++) {
+        liElements.item(i).addEventListener('mouseover', textColor);
+        liElements.item(i).addEventListener('mouseout', out);
+    }
+})();
