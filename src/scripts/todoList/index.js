0d1626f1926f57e36a29e1e6e93bac456b56f3c8
AntonEremeenko <antoneremeenkopal@gmail.com>
2024-06-17 18:41:10 +0300

    hw_71 not work

---
+++ src/scripts/todoList/index.js
@@ -0,0 +1,12 @@
+import Model from './Model.js';
+import View from './View.js';
+import Controller from './Controller.js';
+
+const selectors = {
+    form: '#todoForm',
+    todosContainer: '#todoItems',
+};
+
+View.init(selectors);
+Model.init();
+Controller.init(selectors);
